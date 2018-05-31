a81bcf215f2871c087010c02b1091e6333a5b60f
Maria E. V. Neptune <maria.elysse.n@gmail.com>
2018-05-31 12:28:37 -0700

    mistakes were made

---
+++ insn_mem_16x256.v
@@ -0,0 +1,19 @@
+module insn_mem_16x256 (
+  input  wire        clk, 
+  input  wire        rst, 
+  input  wire [7:0]  ra, 
+  output reg  [15:0] rd
+  );
+`include "includes.v" //all the letter positions
+
+  reg [15:0] ram [0:254];
+
+  always @ (posedge clk) begin
+    if (rst) begin
+      $readmemb("program.archel", ram, 0, 254);
+      rd <= 16'h0000;
+    end
+    else
+      rd <= ram[ra];
+  end
+endmodule // insn_mem_16x256
