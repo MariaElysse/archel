ca5f32e0c39c3ad89f512f9ae19deb1f9ecd0b3d
Michael Liu <mike666234@gmail.com>
2018-05-31 07:17:11 -0700

    Get pause switch & step button working, fix debouncer

--- debouncer.v
+++ debouncer.v
@@ -2,16 +2,22 @@
 
 module debouncer(
   input wire clk,
+  input wire rst,
   input wire btn,
   output reg btn_state,
   output wire btn_down, // pulse 1cy
   output wire btn_up // pulse 1cy
   );
   
-  reg [63:0] db_counter;
+  reg [5:0] db_counter;
   
   always @ (posedge clk) begin
-    if (btn == 0) db_counter <= 0;
+    if (rst) begin
+      btn_state <= 0;
+      db_counter <= 0;
+    end
+    else if (btn == 0)
+      db_counter <= 0;
     else begin
       db_counter <= db_counter + 1;
       if (&db_counter) btn_state <= ~btn_state;
