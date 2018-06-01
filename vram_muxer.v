910e454c2641b1641dca8f42e4593d6cce499203
Maria E. V. Neptune <maria.elysse.n@gmail.com>
2018-06-01 00:23:30 -0700

    vram_muxer.v: the module declaration literally does not work because of 2-dimensionality rtfix

--- vram_muxer.v
+++ vram_muxer.v
@@ -22,21 +22,23 @@ module vram_muxer( //select one of the inputs to be activated and passed on to t
 		input clk,
 		input rst,
 		
-		input wire  [8:0] vram_addr [7:0], //from the module that wants to do the write
-		input wire [639:0] write_vram [7:0],
-		input wire write_active [7:0],
-		output wire [7:0] active_writer, //to the module wanting to do the write (wait ur turn lol)
-		
+		input wire [7:0] [8:0] vram_addr , //from the module that wants to do the write
+		input wire [7:0] [639:0] write_vram,
+		input wire [7:0] write_active,
+		output wire[7:0] active_writer, //to the module wanting to do the write (wait ur turn lol)
+		output wire [7:0] [639:0] read_vram,
 		output wire [639:0] to_vram_write,
+		input wire  [639:0] from_vram_read,
 		output wire [8:0] to_vram_addr,
 		output wire to_vram_wea
    );
-	reg active_writer_n[3:0] = 0;
+	reg [3:0] active_writer_n = 0;
 	
 	assign active_writer = 1'b1 << active_writer_n;
 	assign to_vram_write = write_vram[active_writer_n];
 	assign to_vram_addr = vram_addr[active_writer_n];
 	assign to_vram_wea = write_active[active_writer_n];
+	assign from_vram_read = from_vram_read[active_writer_n];
 	
 	always @ (posedge clk) begin 
 		active_writer_n = active_writer_n + 1;
