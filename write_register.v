25a4e18875c57e24a537aa8698d66688f2d0732c
Maria E. V. Neptune <maria.elysse.n@gmail.com>
2018-06-01 00:12:56 -0700

    the ipcore bs that i didnt change and yet has 10000 git changes

--- write_register.v
+++ write_register.v
@@ -19,7 +19,35 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module write_register(
+	input wire clk,
+	
+	input wire turn,
+	input wire [639:0] lfrom_vram,
+	output wire [8:0] vram_addr,
+	input wire [9:0] x_pos,
+	input wire [8:0] y_pos,
+	output wire activate_write,
+
+	input wire vram_turn,
+	input wire [3:0] regnum,
+	input wire [15:0] regdat,
+	output wire reg_done
     );
-
-
+	reg [3:0] letter_n_of_string; //which letter we're writing R_. FFFF
+	reg [5:0] letter; //which letter we're writing (abcdef...)
+	reg let_done;
+	write_letter w_let(
+		.clk(clk),
+		.let(letter),
+		
+		.x_pos(x_pos + letter_n_of_string * 7),
+		.y_pos(y_pos),
+		.line_from_vram(lfrom_vram),
+		.line_addr(vram_addr),
+		.activate_write(activate_write),
+		
+		.let_done(let_done)
+	);
+	
+	always @(
 endmodule
